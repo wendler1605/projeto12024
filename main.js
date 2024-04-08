2ef26d0f5eae5fcfff497c539f1a84f05bd1b637
wendler1605 <160506472+wendler1605@users.noreply.github.com>
2024-04-08 11:47:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i <botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+    botoes[i].classList.remove("ativo");
+    textos[i].classList.remove("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date ("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date ("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date ("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date ("2024-02-01T00:00:00");
+
+const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function cauculaTempo(tempoObjetivo) {
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
