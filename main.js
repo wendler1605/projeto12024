bc63a582bbdad4e0d038e2f68246af51ae316ce3
wendler1605 <160506472+wendler1605@users.noreply.github.com>
2024-04-15 11:31:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -39,3 +39,19 @@ if (tempoFinal > 0){
 } else {
     return "Prazo finalizado";
 }
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0]
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1]
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2]
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3]
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+comecaCronometro();
